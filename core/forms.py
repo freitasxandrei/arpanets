791f118e110f3a132051ecbbf4e80b1c5010d839
Andrei de Freitas <freitasxandrei@gmail.com>
2025-04-07 19:44:05 -0300

    ajustesEntregaParcial

--- core/forms.py
+++ core/forms.py
@@ -3,9 +3,11 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 class RegisterForm(UserCreationForm):
-    gender = forms.CharField(max_length=10)
-    age = forms.IntegerField()
+    gender = forms.CharField(max_length=10, label='Gênero')
+    age = forms.IntegerField(label='Idade')
 
     class Meta:
         model = User
-        fields = ['username', 'password', 'gender', 'age']
+        fields = ['username', 'password1', 'password2', 'gender', 'age']
+
+    
